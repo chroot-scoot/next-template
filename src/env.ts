bda4f2bd731fc1ab5f9b817464bfafd4ea0018da
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-04-26 21:11:24 +0530

    chore: added supabase

--- src/env.ts
+++ src/env.ts
@@ -3,14 +3,17 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    DATABASE_CONN_STR: z.string(),
     LOGSTREAM_SOURCE_TOKEN: z.string(),
     NODE_ENV: z.enum(['production', 'development']),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+  },
   runtimeEnv: {
-    DATABASE_CONN_STR: process.env.DATABASE_CONN_STR,
     LOGSTREAM_SOURCE_TOKEN: process.env.LOGSTREAM_SOURCE_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 });
